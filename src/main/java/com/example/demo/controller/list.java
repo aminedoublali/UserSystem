b48e2b0336deadf945eb681cf8f50930a1e7c756
yuki <yuki.kokuho@g.dreamcareer.co.jp>
2023-05-29 17:41:09 +0900

    (tentative)UPDATE use data access object class AccountDao to AccountService

--- src/main/java/com/example/demo/controller/list.java
+++ src/main/java/com/example/demo/controller/list.java
@@ -6,18 +6,26 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 
 import com.example.demo.Entity.Account;
-import com.example.demo.dao.AccountDao;
+import com.example.demo.service.AccountService;
 
 public class list {
-	private final AccountDao accountDao;
-
+	/*	private final AccountDao accountDao;
+	
+		@Autowired
+		list(AccountDao accountdao) {
+			this.accountDao = accountdao;
+		}
+	*/
+	
+	private final AccountService accountService;
+	
 	@Autowired
-	list(AccountDao accountdao) {
-		this.accountDao = accountdao;
+	list(AccountService accountService){
+		this.accountService = accountService;
 	}
-
+	
 	public String list(Model model, Account account) {
-		List<Account> list = accountDao.findAll();
+		List<Account> list = accountService.getAllAccounts();
 		model.addAttribute("acList", list);
 		return "list";
 	}
