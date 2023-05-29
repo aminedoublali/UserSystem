260d9b85cd41e2087c4aa4743dce25462179de55
githamu <haruto030201@gmail.com>
2023-05-29 15:49:43 +0900

    テストデータをdata.sqlとして追加

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO Account (name, email, password, address, deleted) VALUES
+('1', '1@example.com', 'password1', 'address1', false),
+('2', '2@example.com', 'password2', 'address2', false),
+('3', '3@example.com', 'password3', 'address3', false);
