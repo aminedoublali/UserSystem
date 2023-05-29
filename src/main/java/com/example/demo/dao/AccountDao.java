fe453191fbd0b332cb095e7a829a204655d687c2
DESKTOP-DCRKJSS\ITS-50 <takuya.kitagaki@g.dreamcareer.co.jp>
2023-05-29 15:03:59 +0900

    update

--- src/main/java/com/example/demo/dao/AccountDao.java
+++ src/main/java/com/example/demo/dao/AccountDao.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.example.demo.Entity.Account;
+
 @Repository
 public interface AccountDao extends JpaRepository<Account, Long> {
     
--- src/main/java/com/example/demo/service/AccountService.java
+++ src/main/java/com/example/demo/service/AccountService.java
@@ -3,15 +3,15 @@ package com.example.demo.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.Repository;
 import org.springframework.stereotype.Service;
 
+import com.example.demo.Entity.Account;
 import com.example.demo.dao.AccountDao;
 
 @Service
 public class AccountService {
 	
-    private final Repository accountdao;
+    private final AccountDao accountdao;
 
     @Autowired
     public AccountService(AccountDao accountdao) {
