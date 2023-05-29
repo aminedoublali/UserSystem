5f31820c1fb1962ba59ea06e301e1fc63f674e8f
DESKTOP-DCRKJSS\ITS-50 <takuya.kitagaki@g.dreamcareer.co.jp>
2023-05-29 17:07:06 +0900

    Login update

--- src/main/java/com/example/demo/LoginForm.java
+++ src/main/java/com/example/demo/LoginForm.java
@@ -1,8 +1,12 @@
 package com.example.demo;
 
+import jakarta.validation.constraints.NotEmpty;
+
 public class LoginForm {
 
+	@NotEmpty(message="メールアドレスを入力してください。")
 	private String email;
+	@NotEmpty(message="パスワードを入力してください。")
 	private String password;
 	
 	public LoginForm() {}
--- src/main/java/com/example/demo/controller/LoginController.java
+++ src/main/java/com/example/demo/controller/LoginController.java
@@ -1,19 +1,42 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.demo.LoginForm;
+import com.example.demo.Entity.Account;
+import com.example.demo.service.AccountService;
 
 @Controller
 public class LoginController {
 	
+	@Autowired
+	AccountService service;
+	
+	public LoginController(AccountService service) {
+		this.service = service;
+	}
+	
 	@RequestMapping("/login")
-	public String login(Model model) {
+	public String login(Model model,LoginForm form) {
 		return "login";
 	}
 	
 	@RequestMapping("/mypage")
-	public String loginComplete(Model model) {
+	public String mypage(@Validated LoginForm form, BindingResult result,Model model) {
+		if(result.hasErrors()) {
+			return "login";
+		}
+		Account ac = service.login(form.getEmail());
+		boolean error = service.logincheck(ac, form.getPassword(), model);
+		if(error) {
+			return "login";
+		}
+		model.addAttribute("account", ac);
 		return "mypage";
 	}
 }
