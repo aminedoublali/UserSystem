3f1771c0e8cada6170c69c081c5ee55b4298f59d
githamu <haruto030201@gmail.com>
2023-05-29 15:30:09 +0900

    login()とlogincheck()を追加

--- src/main/java/com/example/demo/dao/AccountDao.java
+++ src/main/java/com/example/demo/dao/AccountDao.java
@@ -5,9 +5,13 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.example.demo.Entity.Account;
+
 @Repository
 public interface AccountDao extends JpaRepository<Account, Long> {
     
     List<Account> findAll();
+    
+    Account findByEmail(String email);
     
 }
--- src/main/java/com/example/demo/service/AccountService.java
+++ src/main/java/com/example/demo/service/AccountService.java
@@ -3,15 +3,16 @@ package com.example.demo.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.Repository;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
 
+import com.example.demo.Entity.Account;
 import com.example.demo.dao.AccountDao;
 
 @Service
 public class AccountService {
 	
-    private final Repository accountdao;
+    private final AccountDao accountdao;
 
     @Autowired
     public AccountService(AccountDao accountdao) {
@@ -21,4 +22,26 @@ public class AccountService {
     public List<Account> getAllAccounts() {
         return accountdao.findAll();
     }
+    
+    public Account login(String email) {
+    	
+        Account account = accountdao.findByEmail(email);
+        
+        return account;
+    }
+    
+    public Model logincheck(Account account, String password, Model model) {
+    	
+    	if (account == null) {
+            model.addAttribute("emailerrormessage", "メールアドレスが見つかりませんでした");
+        }
+
+        if (!account.getPassword().equals(password)) {
+        	model.addAttribute("passworderrormessage", "パスワードが間違っています");
+        }
+        
+        return model;
+    	
+    }
+    
 }
