022e46dac358d81553da0f1b2f546b2b733a35cf
yuki <yuki.kokuho@g.dreamcareer.co.jp>
2023-05-30 13:28:26 +0900

    listからアカウント削除可能に

--- src/main/java/com/example/demo/controller/WebController.java
+++ src/main/java/com/example/demo/controller/WebController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.demo.Entity.Account;
@@ -30,5 +31,11 @@ public class WebController {
 		List<Account> list = accountService.getAllAccounts();
 		model.addAttribute("acList", list);
 		return "list";
+	}
+	
+	@RequestMapping("del/{email}")
+	public String del(@PathVariable String email) {
+		accountService.deleteaccount(email);
+		return "redirect:/list";
 	}
 }
